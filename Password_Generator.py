db603a78e29bd88df3c0fa9035d1aa866a0d8cf7
Soumyadeep Ray Samanta <159132811+SoumyadeepRaySamanta@users.noreply.github.com>
2025-03-08 23:44:56 +0530

    Password_Generator.py

--- Password_Generator.py
+++ Password_Generator.py
@@ -1,31 +1,32 @@
+# This is a password generator based on Python 3.
+# Written by Soumyadeep Ray Samanta.
+
 import random
 import string
 
+# List of Characters
 print("Welcome to the Password Generator!")
 lowerCase = string.ascii_lowercase
 upperCase = string.ascii_uppercase
 numbers = string.digits
 symbols = string.punctuation
 
+# If user needs random number of characters:
 userinput = int(input("Enter the length of the password: "))
 
-lowerCase_letters = 0
-upperCase_letters = 0
-N_numbers = 0
-N_symbols = 0
+# Ensuring at least one character from each category
+lowerCase_letters = random.randint(1, userinput - 3)
+upperCase_letters = random.randint(1, userinput - lowerCase_letters - 2)
+N_numbers = random.randint(1, userinput - lowerCase_letters - upperCase_letters - 1)
+N_symbols = userinput - (lowerCase_letters + upperCase_letters + N_numbers)  # Remaining characters
 
-lowerCase_letters += random.randint(1, userinput+1)
-userinput -= lowerCase_letters
+# If user needs particular number of characters:
+# lowerCase_letters = int(input("Number of lowercase letters you like in your password: "))
+# upperCase_letters = int(input("Number of uppercase letters you like in your password: "))
+# N_numbers = int(input("Number of numbers you like in your password: "))
+# N_symbols = int(input("Number of symbols you like in your password: "))
 
-upperCase_letters += random.randint(1, userinput+1)
-userinput -= upperCase_letters
-
-N_numbers += random.randint(1, userinput+1)
-userinput -= N_numbers
-
-N_symbols += random.randint(1, userinput+1)
-userinput -= N_symbols
-
+# Generate Password
 password_list = []
 
 for char in range(0, lowerCase_letters):
@@ -40,6 +41,7 @@ for char in range(0, N_numbers):
 for char in range(0, N_symbols):
     password_list += random.choice(symbols)
 
+# Shuffle Password
 random.shuffle(password_list)
 
 password = ""
